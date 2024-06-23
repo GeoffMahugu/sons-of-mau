7ea1776acf9bc748a79441d21fd15f1fb85c8ae3
GeoffMahugu <geoffreymahugu@gmail.com>
2024-06-24 01:28:05 +0300

    initialize shadcn,

--- app/page.js
+++ app/page.js
@@ -47,27 +47,7 @@ export default function Home() {
 
   return (
     <main className='flex relative  min-h-screen h-screen flex-col items-center justify-between p-24 bg-white'>
-      {/* <div
-        id='landing-section'
-        className='flex flex-col items-center h-inherit p-10 '
-      >
-        <h1 className='text-8xl'>The Revolution starts here !</h1>
-      </div> */}
-      <div
-        id='landing-section'
-        className='flex flex-col items-center h-screen p-10 '
-      >
-        <h1 className='text-8xl text-black'>The Revolution starts here !</h1>
-      </div>
-
-      <div className='flex flex-col min-h-screen py-80 gap-5'>
-        <div className='flex flex-col'>
-          <h1>
-            MPs who Voted <bold>YES</bold>{" "}
-          </h1>
-        </div>
-        <div className='flex flex-col min-h-screen py-80 gap-5'></div>
-      </div>
+      <h1>MPS who Voted Yes</h1>
     </main>
   );
 }
